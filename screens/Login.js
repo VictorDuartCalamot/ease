2937820dd66464fae3657539266c288453a7284a
Victor <victor.duart@ieselcalamot.com>
2023-10-25 12:12:17 +0200

    Login UI updated

--- screens/Login.js
+++ screens/Login.js
@@ -1,50 +1,110 @@
-import { Text, StyleSheet, View, Image } from 'react-native';
-import React, { useState } from 'react';
+import {
+    Text,
+    StyleSheet,
+    View,
+    Image,
+    TextInput,
+    TouchableOpacity
+} from 'react-native';
+import React, {useState} from 'react';
 
-export default function Login() { 
-  
+export default function Login() {
+
     return (
-        <View style={styles.padre}>
+        <View style={styles.main}>
 
             <View>
-                <Image source={require('../assets/logo.png')} style={styles.profile} />
+                <Image source={require('../assets/logo.png')} style={styles.logo}/>
             </View>
-        
-            <View style={styles.tarjeta}>  
 
+            <View style={styles.login_box}>
+
+                <View style={styles.textBox}>
+                    <TextInput
+                        placeholder='email@gmail.com'
+                        style={{
+                        paddingHorizontal: 15
+                    }}></TextInput>
+
+                </View>
+
+                <View style={styles.textBox}>
+                    <TextInput
+                        placeholder='password'
+                        style={{
+                        paddingHorizontal: 15
+                    }}></TextInput>
+                </View>
+
+                <View style={styles.mainButton}>
+                    <TouchableOpacity style={styles.button_box}>
+
+                        <Text style={styles.textButton}>Sign In</Text>
+
+                    </TouchableOpacity>
+                </View>
             </View>
-                            
+
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-    padre:{
-        flex:1,
-        justifyContent:'center',
-        alignItems:'center',
-        backgroundColor:'white',
-
+    main: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#FCFCFC'
     },
-    profile:{
-        width:100,
-        height:100,
-        borderRadious:50,
-        borderColor:'black',
-    },
-    tarjeta:{
-        margin:20,
-        backgroundColor:'white',
-        //borderRadious:20,
-        width:'90%',
-        padding:20,
-        shadowColor:'#000',
-        shadowOffset:{
-            width:0,
-            height:2
+    logo: {
+        width: 100,
+        height: 100,
+        marginBottom: 50,
+        borderRadius: 50,
+        borderColor: 'black',
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2
         },
-        shadowOpacity:0.25,
-        shadowRadius:4,
-        elevation:5,
+        shadowOpacity: 0.25,
+        shadowOpacity: 0.25,
+        shadowRadius: 4
+    },
+    login_box: {
+        margin: 5,
+        backgroundColor: 'white',
+        borderRadius: 20,
+        width: '90%',
+        padding: 20,
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5
+    },
+    textBox: {
+        paddingVertical: 20,
+        backgroundColor: '#cccccc40',
+        borderRadius: 30,
+        marginVertical: 10
+    },
+    mainButton: {
+        alignItems: 'center'
+    },
+    button_box: {
+        backgroundColor: '#525FE1',
+        borderRadius: 30,
+        paddingVertical: 20,
+        width: 150,
+        marginTop: 20
+    },
+    textButton: {
+        textAlign: 'center',
+        color: 'white'
     }
+
 });
