f9860200d227d2e1f60d0893ed39e6243787ce03
Victor Duart <victor.duart@ieselcalamot.com>
2023-11-02 16:23:07 +0100

    Drawer Added on home screen

--- src/screens/main/Home.js
+++ src/screens/main/Home.js
@@ -1,17 +1,28 @@
-import { Text, StyleSheet, View } from 'react-native';
+import { Text, StyleSheet, View, Button } from 'react-native';
 import React from 'react';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
 
-export default function Home() { 
+import SettingsScreen from '../settings/user_settings';
   
-    return (
-        <View style={styles.mainStyle}>
-            <View>
-                <Text style={{textAlign:'center'}}>WIP</Text>
-            </View>
-        </View>
-    )
+  const Drawer = createDrawerNavigator();
+
+function HomeScreen() {
+  <view>
+    hola
+  </view>
 }
 
+export default function Home() {
+    return(
+        <NavigationContainer independent={true}>
+          <Drawer.Navigator initialRouteName='Home'>
+            <Drawer.Screen name="Home" component={HomeScreen} />          
+            <Drawer.Screen name="Settings" component={SettingsScreen} />        
+          </Drawer.Navigator>  
+    </NavigationContainer>
+    )
+}
 
 const styles = StyleSheet.create({
     mainStyle: {
